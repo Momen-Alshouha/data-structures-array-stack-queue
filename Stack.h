c8a6b52839b214d27f9a65f32510b5df9aa092fb
momen-alshouha <momenalshouha@outlook.com>
2024-01-23 20:00:55 +0300

    - constructors added - capacity methods added - error handling

--- Stack.h
+++ Stack.h
@@ -9,32 +9,52 @@ private:
 
 public:
 
+	Stack() = default;
+
+	Stack(size_t initialCapacity) : stack(initialCapacity) {}
+
 	size_t Size() {
 		return stack.size;
 	}
 
 	T Top() {
+		if (IsEmpty()) {
+			throw std::out_of_range("Stack is empty");
+		}
 		return stack.At(0);
 	}
 
 	T Bottom() {
+		if (IsEmpty()) {
+			throw std::out_of_range("Stack is empty");
+		}
 		return stack.At(stack.size-1);
 	}
 
 	T Peek() {
-		return Top()
+		return Top();
 	}
 
 	void push(T value) {
+		if (IsEmpty()) {
+			throw std::out_of_range("Stack is empty");
+		}
 		stack.InsertBegin(value);
 	}
 
 	void pop() {
+		if (IsEmpty()) {
+			throw std::out_of_range("Stack is empty");
+		}
 		stack.RemoveAtIndex(0);
 	}
 
 	bool IsEmpty() {
 		return this->IsEmpty();
+	}
+
+	size_t Capacity() {
+		return stack.capacity;
 	}
 
 	void print() {
